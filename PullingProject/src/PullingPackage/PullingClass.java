2d690fadff0b48abec7ab3d83cdc5cbd6648ef4d
Naveenkumar17th <navveen17th@gmail.com>
2024-01-11 11:49:05 +0530

    Thirdline replaced with firstline

--- PullingProject/src/PullingPackage/PullingClass.java
+++ PullingProject/src/PullingPackage/PullingClass.java
@@ -5,10 +5,12 @@ public class PullingClass {
 	public static void main(String[] args) {
 
     
-    System.out.println("hellow naveen");
+    
    System.out.println("hellow kumar");
 
-   System.out.println("hellow World");		
+   System.out.println("hellow World");	
+   
+   System.out.println("hellow naveen");
 
 	}
 
